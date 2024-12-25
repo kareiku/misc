f8a0002f42c2689c2530bd249650ceb45a0430e7
kareiku <xaxvu7@gmail.com>
2024-12-25 23:16:47 +0100

    Empty commit

--- scripts/gqu.bash
+++ scripts/gqu.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # gqu stands for "git quick update"
 git add --all
-commit_message=${1:-"Empty commit message"}
+commit_message=${1:-"empty commit message"}
 git commit -m "$commit_message"
 git push

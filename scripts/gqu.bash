a8d9a03c134e00ba85af7deab6039fae95d6ead7
kareiku <xaxvu7@gmail.com>
2024-12-25 23:40:49 +0100

    empty commit message

--- scripts/gqu.bash
+++ scripts/gqu.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 # gqu stands for "git quick update"
+git rev-parse --is-inside-work-tree > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
 git add --all
 commit_message=${1:-"empty commit message"}
 git commit -m "$commit_message"
 git push
+else echo ".git directory not found."
+fi

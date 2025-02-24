85eaab914574479cd11cbe2c62047f7a18b31e82
kareiku <xaxvu7@gmail.com>
2025-02-24 01:08:12 +0100

    Changed everything in docs to be a bit better than before

--- docs/script.js
+++ docs/script.js
@@ -1,28 +1,36 @@
-let originalData = null;
-fetch("./data.json").then(response => response.json()).then(data => filterTable(originalData = data)).catch(error => console.error(error));
+let entries = null;
 
-function populateTable(data) {
-    const table = document.getElementById("results");
-    const header = table.rows[0];
+const table = document.getElementById("results");
+const field = document.getElementById("selector");
+const input = document.getElementById("search-bar");
+
+window.addEventListener("load", () => {
+    fetch("./data")
+        .then(response => response.json())
+        .then(data => entries = data)
+        .then(() => loadTable())
+        .then(() => input.focus())
+        .catch(error => console.error(error));
+});
+
+field.addEventListener("mousedown", () => input.focus());
+input.addEventListener("keyup", () => loadTable());
+
+function loadTable() {
+    const filteredData = input !== "" ? entries.filter(entry => (entry[field.value] || "").toLowerCase().includes(input.value.toLowerCase())) : entries;
+    const keys = entries && entries.length > 0 ? Object.keys(entries[0]) : [];
     table.innerHTML = "";
-    table.appendChild(header);
-    data.forEach(record => {
+
+    const header = document.createElement("tr");
+    keys.forEach(key => {
+        const th = document.createElement("th");
+        th.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+        header.appendChild(th);
+    });
+    table.append(header);
+
+    filteredData.forEach(datum => {
         const row = table.insertRow();
-        record = [record.type, record.for, record.name, record.url];
-        record.forEach(field => {
-            const cell = row.insertCell();
-            cell.innerHTML = field === false ? false : field || "";
-            const URLMatch = cell.innerHTML.match(/https?:\/\/[^\s]+/);
-            if (URLMatch) {
-                cell.innerHTML = `<a href=${cell.innerHTML}>${cell.innerHTML}</a>`;
-            }
-        });
+        keys.forEach(key => row.insertCell().innerHTML = datum[key]);
     });
 }
-
-function filterTable() {
-    const column = document.getElementById("selector").value;
-    const query = document.getElementById("filter").value;
-    populateTable(query ? originalData.filter(record => (record[column] || "").toLowerCase().includes(query.toLowerCase())) : originalData);
-}
-

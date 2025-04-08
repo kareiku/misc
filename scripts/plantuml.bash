dcdfec8dcbdbfa5b91ffb96e69f24b2aef62b7e1
kareiku <xaxvu7@gmail.com>
2025-04-08 11:54:13 +0200

    feat: add script to run plantuml with java

---
+++ scripts/plantuml.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+    echo "Usage: $0 <plantuml file>"
+    exit
+fi
+
+if [ ! -f ~/.plantuml.jar ]
+then wget -q -O ~/.plantuml.jar 'https://github.com/plantuml/plantuml/releases/download/v1.2025.2/plantuml-mit-1.2025.2.jar'
+fi
+
+java -jar ~/.plantuml.jar $1

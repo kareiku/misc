fd79ef6c9cbd716f7fddb9543a49ef251f833e49
kareiku <xaxvu7@gmail.com>
2025-04-27 05:49:27 +0200

    perf: allow for multiple urls as arguments

--- scripts/yt-dlp/dlalbum.bash
+++ scripts/yt-dlp/dlalbum.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [ -z $1 ]
-then echo "Usage: $0 URL"
-else yt-dlp -q --progress --no-warnings -x $1 -o './%(album)s/%(track_number)s-%(track)s.%(ext)s'
+then echo "Usage: $0 URL [URL...]"
+else
+    for url in $@
+    do yt-dlp -q --progress --no-warnings -x $url -o './$(artist)s/%(album)s/%(track_number)s-%(track)s.%(ext)s'
+    done
 fi
--- scripts/yt-dlp/dlplaylist.bash
+++ scripts/yt-dlp/dlplaylist.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [ -z $1 ]
-then echo "Usage: $0 URL"
-else yt-dlp -q --progress --no-warnings -x $1 -o './%(playlist)s/%(playlist_index)s-%(title)s.%(ext)s'
+then echo "Usage: $0 URL [URL...]"
+else
+    for url in $@
+    do yt-dlp -q --progress --no-warnings -x $url -o './$(artist)s/%(playlist)s/%(playlist_index)s-%(title)s.%(ext)s'
+    done
 fi

2976ff40d7bcee8968910928da2525ad1e197098
kareiku <xaxvu7@gmail.com>
2025-04-27 21:29:06 +0200

    chore: rename file and edit output

---
+++ scripts/yt-dlp/concurrent.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+################# IMPORTANT NOTE #################
+#                                                #
+# If inputting a file with space-separated urls, #
+# run the script in the following way:           #
+#                                                #
+#      xargs concurrent.bash < urls.txt          #
+#                                                #
+##################################################
+
+M=3
+
+while [[ $# > 0 ]]
+do
+    for ((i = 0; i < M && $# > 0; i++))
+    do
+        yt-dlp -x $1 -o './%(channel)s/%(playlist)s/%(playlist_index)s-%(title)s.%(ext)s'
+        shift
+    done
+    wait
+done

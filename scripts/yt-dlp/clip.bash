cd36679b44b9cd224ca1cf25a0ecd04e2e1cc85a
kareiku <xaxvu7@gmail.com>
2025-04-28 13:20:18 +0200

    docs: change usage message

--- scripts/yt-dlp/clip.bash
+++ scripts/yt-dlp/clip.bash
@@ -2,7 +2,7 @@
 
 if [[ -z $1 || -z $2 || -z $3 ]]
 then
-    echo "Usage: $0 START_TIME END_TIME URL"
+    echo "Usage: $0 <start_time> <end_time> <url>"
     echo "Time format: \"HH:mm:ss.s\""
 else
     yt-dlp --downloader ffmpeg --downloader-args "ffmpeg_i:-ss $1 -to $2" $3

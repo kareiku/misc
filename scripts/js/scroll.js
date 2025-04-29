d4eeebc0c38443e24765d15a36cce3d6ec339e27
kareiku <xaxvu7@gmail.com>
2025-04-29 05:02:06 +0200

    chore: remove old file

--- scripts/js/scroll.js
+++
@@ -1,5 +0,0 @@
-let currentScroll = 0;
-const scrollInterval = setInterval(() => {
-    window.scrollTo(0, document.body.scrollHeight);
-    if (++currentScroll >= 15) clearInterval(scrollInterval);
-}, 2000);

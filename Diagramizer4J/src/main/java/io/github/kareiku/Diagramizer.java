002439d84f528690ce342f976076c488691ce8b8
kareiku <xaxvu7@gmail.com>
2025-05-04 01:42:25 +0200

    fix(Diagramizer4J): fix constructors output

--- Diagramizer4J/src/main/java/io/github/kareiku/Diagramizer.java
+++ Diagramizer4J/src/main/java/io/github/kareiku/Diagramizer.java
@@ -3,13 +3,12 @@ package io.github.kareiku;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.Modifier;
-import java.lang.reflect.Parameter;
+import java.lang.reflect.*;
 import java.util.Arrays;
 import java.util.Objects;
 
 public class Diagramizer {
-    public static String diagramize(@NotNull Class<?> clazz, @Nullable String stereotype, @Nullable String alias) {
+    public static @NotNull String diagramize(@NotNull Class<?> clazz, @Nullable String stereotype, @Nullable String alias) {
         StringBuilder sb = new StringBuilder();
 
         stereotype = Objects.isNull(stereotype) ? "" : String.format("<<%s>>\\n", stereotype);
@@ -17,54 +16,18 @@ public class Diagramizer {
 
         sb.append("object \"").append(stereotype).append(clazz.getSimpleName()).append("\" as ").append(alias).append(" {\n");
 
-        Arrays.stream(clazz.getDeclaredFields()).filter(field -> !field.isSynthetic()).forEach(field -> sb
-                .append("    ")
-                .append(getModifierSymbol(field.getModifiers()))
-                .append(field.getName())
-                .append(": ")
-                .append(field.getType().getSimpleName())
-                .append("\n")
-        );
-
+        Arrays.stream(clazz.getDeclaredFields())
+                .filter((field) -> !field.isSynthetic())
+                .forEach((field) -> sb.append(formatField(field)));
         sb.append("    ---\n");
 
-        Arrays.stream(clazz.getDeclaredConstructors()).filter(constructor -> !constructor.isSynthetic()).forEach(constructor -> {
-            sb.append("    ")
-                    .append(getModifierSymbol(constructor.getModifiers()))
-                    .append(constructor.getName())
-                    .append('(');
+        Arrays.stream(clazz.getDeclaredConstructors())
+                .filter((constructor) -> !constructor.isSynthetic())
+                .forEach((constructor) -> sb.append(formatConstructor(constructor)));
 
-            Parameter[] params = constructor.getParameters();
-            for (int i = 0; i < params.length; i++) {
-                if (i > 0) sb.append(", ");
-                sb.append(params[i].getName())
-                        .append(": ")
-                        .append(params[i].getType().getSimpleName());
-            }
-
-            sb.append(')')
-                    .append('\n');
-        });
-
-        Arrays.stream(clazz.getDeclaredMethods()).filter(method -> !method.isSynthetic()).forEach(method -> {
-            sb.append("    ")
-                    .append(getModifierSymbol(method.getModifiers()))
-                    .append(method.getName())
-                    .append("(");
-
-            Parameter[] params = method.getParameters();
-            for (int i = 0; i < params.length; i++) {
-                if (i > 0) sb.append(", ");
-                sb.append(params[i].getName())
-                        .append(": ")
-                        .append(params[i].getType().getSimpleName());
-            }
-
-            sb.append("): ")
-                    .append(method.getReturnType().getSimpleName())
-                    .append("\n");
-
-        });
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter((method) -> !method.isSynthetic())
+                .forEach((method) -> sb.append(formatMethod(method)));
 
         sb.append("}");
 
@@ -76,5 +39,41 @@ public class Diagramizer {
         else if (Modifier.isProtected(modifiers)) return '#';
         else if (Modifier.isPrivate(modifiers)) return '-';
         else return '~';
+    }
+
+    private static @NotNull String formatField(@NotNull Field field) {
+        return String.format("    %c%s: %s\n",
+                getModifierSymbol(field.getModifiers()),
+                field.getName(),
+                field.getType().getSimpleName());
+    }
+
+    private static @NotNull String formatConstructor(@NotNull Constructor<?> constructor) {
+        return String.format("    %c%s(%s)\n",
+                getModifierSymbol(constructor.getModifiers()),
+                constructor.getName(),
+                formatParameters(constructor));
+    }
+
+    private static @NotNull String formatMethod(@NotNull Method method) {
+        return String.format("    %c%s(%s): %s\n",
+                getModifierSymbol(method.getModifiers()),
+                method.getName(),
+                formatParameters(method),
+                method.getReturnType().getSimpleName());
+    }
+
+    private static @NotNull String formatParameters(@NotNull Executable executable) {
+        StringBuilder sb = new StringBuilder();
+        Parameter[] params = executable.getParameters();
+
+        for (int i = 0; i < params.length; i++) {
+            if (i > 0) sb.append(", ");
+            sb.append(params[i].getName())
+                    .append(": ")
+                    .append(params[i].getType().getSimpleName());
+        }
+
+        return sb.toString();
     }
 }

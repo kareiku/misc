8f6a8f67c0332eda1018ec01b0168929062aae61
kareiku <xaxvu7@gmail.com>
2025-08-19 16:49:26 +0200

    Update script
    Remove concurrency to avoid locking

--- scripts/bash/updateall.sh
+++ scripts/bash/updateall.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-(
-    apt update -y && \
-    apt full-upgrade -y && \
-    apt autoremove -y && \
-    apt autoclean -y
-) &
+apt update -y && \
+apt full-upgrade -y && \
+apt autoremove -y && \
+apt autoclean -y
 
-snap refresh &
-
-wait
+snap refresh
 
 if [[ -f /var/run/reboot-required ]]
 then
